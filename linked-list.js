6ec1a9262d0a7c809c4ba3ae0d6555e7e1f22573
bigjunglex <gearl00taz@gmail.com>
2024-05-23 20:28:27 +0300

    finished :zap:

--- linked-list.js
+++ linked-list.js
@@ -1,8 +1,7 @@
 import Node from "./node.js"
 
-function LinkedList () {
+function linkedList () {
     let head = null
-
 
     const append = (value) => {
         const subjNode = new Node(value);
@@ -10,33 +9,118 @@ function LinkedList () {
             head = subjNode
         } else {
             if (head['next'] === null){
-                head['next'] === subjNode
+                head['next'] = subjNode
             } else {
                 let lastNode = head
-                while (!lastNode['next']) {
+                while (lastNode['next'] !== null) {
                     lastNode = lastNode['next']
                 }
                 lastNode['next'] = subjNode;
             }
         }
-    } 
     
+    }    
     
-    const prepend = (value) => {
-        if(head === null){
-            const subjNode = new Node(value);
-            head = subjNode
+    const prepend = (value) => head['item'] === null ? head = new Node(value) : head = new Node(value, head);
+
+    const size = () => {
+        if (head === null) return 0
+        let count = 1,
+            lastNode = head;
+        while (lastNode['next'] !== null){
+            lastNode = lastNode['next']
+            count++
+        }
+        return count
+    }   
+    
+    const getHead = () => head
+    
+    const getTail = () => {
+        let lastNode = head;
+        while (lastNode['next'] !== null){
+            lastNode = lastNode['next']
+        }
+        return lastNode
+    }
+
+    const at = (index) => {
+        let currentIndex = 1,
+            lastNode = head;
+        while(index !== currentIndex){
+            if (lastNode['next'] === null) return 'invalid index'
+            lastNode = lastNode['next']
+            currentIndex++
+        }
+        return lastNode
+    }
+
+    const pop = () => {
+        const prevNode = at(size() - 1);
+        prevNode['next'] = null
+    }
+
+    const contains = (value) => {
+        for (let i = 1; i <= size(); i++){
+            const currentNode = at(i);
+            if (currentNode['item']  === value) return true
+        }
+
+        return false
+    }
+
+    const find = (value) => {
+        for (let i = 1; i <= size(); i++){
+            const currentNode = at(i);
+            if (currentNode['item']  === value) return i
+        }
+
+        return null
+    }
+
+    const toString = () => {
+        let nodeStr = '';
+        for (let i = 1; i <= size(); i++){
+            const currentNode = at(i);
+            nodeStr += `( ${currentNode.item} ) -> `
+                        + `${currentNode.next === null ? "null" : ""} `;
+        }
+
+        return nodeStr
     }
 
 
+    const insertAt = (value, index) => {
+        if (index > size() || index < 0) return 
+        const targetNode = new Node(value, at(index))
+        if (index > 1 ) {
+            at(index - 1)['next'] = targetNode;
+        }
+    }
 
-    contains(value) .
-    find(value) 
-    toString 
+    const removeAt = (index) => {
+        if (index > size() || index < 0) return 
+        if (at(index)['next'] === null) {
+            at(index - 1)['next'] =  null
+        } else if (index > 1) {
+            at(index - 1)['next'] = at(index + 1)
+        } else {
+            head = at(index + 1)
+        }
+    }
 
-
-
-insertAt(value, index) that inserts a new node with the provided value at the given index.
-removeAt(index) that removes the node at the given index.
-
+    return {
+        append,
+        prepend,
+        size,
+        getHead,
+        getTail,
+        at,
+        pop,
+        contains,
+        find,
+        toString,
+        insertAt,
+        removeAt
+    }
 } 
